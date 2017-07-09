11ab53ac8b9f5f83a4a14ec149610cb1b53b6962
Ravigopal Kesari <kesariravigopal@gmail.com>
2017-07-09 14:01:10 +0530

    Adds eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+      "jsx-a11y/href-no-hash": "off",
+      "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }]
+    }
+};
