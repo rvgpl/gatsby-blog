ecec715c1ee06f83cb3649aa6d5dfd025f9e38e3
Ravigopal Kesari <kesariravigopal@gmail.com>
2017-07-16 20:51:46 +0530

    Adds a 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Container from '../styled/container';
+
+export default () =>
+  (<Container>
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that does not exist... the sadness.</p>
+  </Container>);
