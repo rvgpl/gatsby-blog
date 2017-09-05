08aa2864e6e6cb14e887ff5c890843fe80dc9e2a
Ravigopal Kesari <kesariravigopal@gmail.com>
2017-09-05 18:57:42 +0530

    Move posts to a separate folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,28 +7,20 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/src/pages`,
-        name: "pages"
+        path: `${__dirname}/posts/`,
+        name: "posts"
       }
     },
     {
       resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
-          // {
-          //   resolve: "gatsby-remark-images",
-          //   options: {
-          //     maxWidth: 590
-          //   }
-          // },
           "gatsby-remark-prismjs",
           "gatsby-remark-copy-linked-files",
           "gatsby-remark-smartypants"
         ]
       }
     },
-    // "gatsby-transformer-sharp",
-    // "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-styled-components"
   ]
